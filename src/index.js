080592bf49bafba25949bb0aecedc1b6a3832b37
Amy Morrisett <morrisettamy@gmail.com>
2022-08-19 17:26:46 -0400

    began working on NewUser-component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+// import App from './App';
+import NewUser from './NewUser'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NewUser />, document.getElementById('root'))
