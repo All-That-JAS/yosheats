4c097c65e8f8a71a73f5e34a760b9f72ce5ffa76
Selina <gooblersell@gmail.com>
2022-08-23 18:11:07 -0400

    working on adding nutrition api to user collections

--- src/components/NutritionAPI.js
+++ src/components/NutritionAPI.js
@@ -1,21 +1,87 @@
 import React, { useState } from 'react';
 import egg from './eggsoutline.png';
-import { fetchNutrition } from '../api/fetchNutrition'
+import { fetchNutrition } from '../api/fetchNutrition';
+import { db } from '../firebase';
+import {
+  collection,
+  getDocs,
+  setDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  query,
+  where,
+} from 'firebase/firestore';
+import { useAuth } from '../contexts/AuthContext';
 
 import './Nutrition.css';
 
 const App = () => {
-  const [query, setQuery] = useState('');
+  const [queryState, setQueryState] = useState('');
   const [nutrition, setNutrition] = useState({});
 
   const search = async (e) => {
     if (e.key === 'Enter') {
-      const data = await fetchNutrition(query);
-      console.log(data);
+      const data = await fetchNutrition(queryState);
+      console.log('hi from search', data);
       setNutrition(data);
-      setQuery('');
+      setQueryState('');
     }
   };
+  const { currentUser } = useAuth();
+
+  const date = new Date();
+  const todayDate =
+    Date().split(' ')[3] +
+    '-' +
+    (date.getMonth() + 1) +
+    '-' +
+    Date().split(' ')[2];
+
+
+const q = query(collection(db, 'user-days'), where('userId', '===', `${currentUser.uid}`));
+ 
+//TODO: DO WE NEED THIS? Get rid of this if unused
+// const userDaysCollection = db
+  //   .collection('user-days')
+  //   .where('userId', '===', `${currentUser.uid}`);
+
+    // if (userDaysCollection)
+/* if this collection includes today's day
+*/
+
+  const dayDoc = doc(db, 'user-days', todayDate);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  async function handleClick() {
+ 
+    //add a check to see uuid
+    // console.log('daydoc', dayDoc);
+    // console.log('inside await - user', currentUser);
+    // console.log('inside await - collection', userDaysCollection);
+
+    await setDoc(dayDoc, {
+      calories: nutrition.items[0].calories,
+      totalFat: nutrition.items[0].fat_total_g,
+      totalCarb: nutrition.items[0].carbohydrates_total_g,
+      protein: nutrition.items[0].protein_g,
+    });
+
+    //TODO: work on getting each doc 
+    const querySnapshot = await getDocs(q);
+    await querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log('query', doc.id, " => ", doc.data());
+      console.log('doc', doc)
+    });
+
+  }
+
+  /* onsubmit - add item to doc with today's date
+   */
 
   return (
     <div className='main-container'>
@@ -23,8 +89,8 @@ const App = () => {
         type='text'
         className='search'
         placeholder='Search...'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        value={queryState}
+        onChange={(e) => setQueryState(e.target.value)}
         onKeyPress={search}
       />
       {nutrition.items && (
@@ -36,6 +102,10 @@ const App = () => {
                 nutrition.items[0].name.slice(1)}
             </h3>
             <h6>
+              {/* 1 serving size = 100g / do some math here
+allow user to toggle (-/+) size
+
+ */}
               <p>Serving Size(g): {nutrition.items[0].serving_size_g}</p>
               <p>Calories: {nutrition.items[0].calories}</p>
               <p>Total Fat(g): {nutrition.items[0].fat_total_g}</p>
@@ -48,6 +118,10 @@ const App = () => {
               <p>Protein(g): {nutrition.items[0].protein_g}</p>
             </h6>
           </div>
+
+          <button type='submit' onClick={handleClick}>
+            Add to Log
+          </button>
 
           <div className='info'>
             <img className='egg-icon' src={egg} alt={'yoshi egg'} />
