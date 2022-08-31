5c190d0193ad2e9625c1e4de32d3bdfca5892064
Selina <gooblersell@gmail.com>
2022-08-30 21:57:11 -0400

    re-order and styled daily log

--- src/components/DailyLog.js
+++ src/components/DailyLog.js
@@ -182,227 +182,255 @@ const DailyLog = () => {
 
   return (
     <>
-      <Container className="mt-3" style={{ width: '100rem' }}>
+      <Container className='mt-3'>
         <Row>
+          <Col> </Col>
           <Col>
             <Card>
               <CardHeader>
-                <Card.Text className="fw-bolder fs-4 text-center my-3">
+                <Card.Text className='fw-bolder fs-4 text-center my-1'>
                   Daily Log
                 </Card.Text>
               </CardHeader>
               <Card.Body>
-                <div>
-                  <Card.Text className="fw-bold fs-5 text-center my-3">
-                    Foods Consumed Today:
-                  </Card.Text>
-                  <Card.Text className="fs-6 text-lowercase">
-                    <ul>
-                      {todaysFoods.map((food) => {
-                        //   TODO: find a way to consolidate food list so it doesn't list multiple of same item
+                <Card.Text className='fs-6 text-lowercase text-center'>
+                  <ul>
+                    {todaysFoods.map((food) => {
+                      //   TODO: find a way to consolidate food list so it doesn't list multiple of same item
 
-                        //   function refactoringFoodList(foodList) {
-                        //   let consolidatedList = [];
-                        //   let foodListKeys = [];
-                        //   let foodListValues = [];
-                        //   foodList.forEach((food) => {
-                        //     foodListKeys.push(Object.keys(food)[0]);
-                        //     foodListValues.push(Object.values(food)[0]);
-                        //   });
-                        //   // console.log('foodList', foodList);
-                        //   // console.log('foodListKeys', foodListKeys);
-                        //   // console.log('foodListValues', foodListValues);
-                        //   for (let i = 0; i < foodListKeys.length; i++) {
-                        //     if (Object.keys(consolidatedList).includes(foodListKeys[i])) {
-                        //       consolidatedList[foodListKeys[i]] += foodListValues[i];
-                        //     } else {
-                        //       consolidatedList[foodListKeys[i]] = foodListValues[i];
-                        //     }
-                        //   }
-                        //   // console.log('consolidatedList', consolidatedList);
-                        //   return consolidatedList;
-                        // }
+                      //   function refactoringFoodList(foodList) {
+                      //   let consolidatedList = [];
+                      //   let foodListKeys = [];
+                      //   let foodListValues = [];
+                      //   foodList.forEach((food) => {
+                      //     foodListKeys.push(Object.keys(food)[0]);
+                      //     foodListValues.push(Object.values(food)[0]);
+                      //   });
+                      //   // console.log('foodList', foodList);
+                      //   // console.log('foodListKeys', foodListKeys);
+                      //   // console.log('foodListValues', foodListValues);
+                      //   for (let i = 0; i < foodListKeys.length; i++) {
+                      //     if (Object.keys(consolidatedList).includes(foodListKeys[i])) {
+                      //       consolidatedList[foodListKeys[i]] += foodListValues[i];
+                      //     } else {
+                      //       consolidatedList[foodListKeys[i]] = foodListValues[i];
+                      //     }
+                      //   }
+                      //   // console.log('consolidatedList', consolidatedList);
+                      //   return consolidatedList;
+                      // }
 
-                        return (
-                          <>
-                            <span>
-                              <strong>{Object.keys(food)[0]}: </strong>
-                              {Object.values(food)[0]} grams
-                            </span>
-                            <br></br>
-                          </>
-                        );
-                      })}
-                    </ul>
-                  </Card.Text>
-                  <Card.Text className="fw-bold fs-5 text-center my-3">
-                    Progress Toward Goals:
-                  </Card.Text>
-                  <Card.Text className=" fs-5 my-3">
-                    <strong>Calories: </strong>
-                    {todaysCalories} calories consumed.<br></br>
-                    <span className="text-lowercase">
-                      {deficitOrSurplus(
-                        userCalories - todaysCalories,
-                        'calorie'
-                      )}
-                    </span>
-                    <br></br>
-                    <div className="progress ">
-                      {calorieProgress > 100 ? (
-                        <div className="progress-over" style={calorieStyle}>
-                          {calorieProgress}%
-                        </div>
-                      ) : (
-                        <div className="progress-done" style={calorieStyle}>
-                          {calorieProgress}%
-                        </div>
-                      )}
-                    </div>
-                  </Card.Text>
-                  <Card.Text className=" fs-5 my-3 mb-1">
-                    <strong>Carbs: </strong>
-                    {todaysCarbs} grams consumed. <br></br>{' '}
-                    <span className="text-lowercase">
-                      {deficitOrSurplus(userCarbs - todaysCarbs, 'carb')}{' '}
-                    </span>
-                    <div className="text-lowercase mt-3">
-                      {carbRecs.length
-                        ? 'Recommended foods to meet carb goals for today:'
-                        : null}
-                    </div>
-                    <div>
-                      {carbRecs.length ? (
-                        <ul>
-                          {carbRecs.map((foodItem) => {
-                            return (
-                              <>
-                                <span className="fs-6 text-lowercase">
-                                  {
-                                    foodItem[Object.keys(foodItem)[0]]
-                                      .servingSize
-                                  }{' '}
-                                  {Object.keys(foodItem)[0]} has{' '}
-                                  {foodItem[Object.keys(foodItem)[0]].carbs} g
-                                </span>
-                                <br></br>
-                              </>
-                            );
-                          })}
-                        </ul>
-                      ) : null}
-                    </div>
-                    <br></br>
-                    <div className="progress">
+                      return (
+                        <>
+                          <span>
+                            <strong>{Object.keys(food)[0]}: </strong>
+                            {Object.values(food)[0]} grams
+                          </span>
+                          <br></br>
+                        </>
+                      );
+                    })}
+                  </ul>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
+        {/*  */}
+        <Row className='mt-3 mb-5' style={{ height: '25rem' }}>
+          <Col >
+            <Card style={{ height: '26rem' }}>
+              <CardHeader>
+                <Card.Text className='fw-bold fs-6 text-center my-2'>
+                  Calories
+                </Card.Text>
+              </CardHeader>
+              <Card.Body>
+                <Card.Text className=' fs-5 my-3'>
+                  <strong>Consumed: </strong>
+                  {todaysCalories} calories<br></br>
+                  <div className='progress mx-auto'>
+                    {calorieProgress > 100 ? (
+                      <div className='progress-over' style={calorieStyle}>
+                        {calorieProgress}%
+                      </div>
+                    ) : (
+                      <div className='progress-done' style={calorieStyle}>
+                        {calorieProgress}%
+                      </div>
+                    )}
+                  </div>
+                  <span className='text-lowercase fs-6'>
+                    {deficitOrSurplus(userCalories - todaysCalories, 'calorie')}
+                  </span>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardHeader>
+                <Card.Text className='fw-bold fs-6 text-center my-2'>
+                  Carbohydrates
+                </Card.Text>
+              </CardHeader>
+              <Card.Body>
+                <Card.Text className=' fs-5 my-3 mb-1'>
+                  <strong>Consumed: </strong>
+                  {todaysCarbs} grams
+                  <span>
+                    <div className='progress mx-auto'>
                       {carbsProgress > 100 ? (
-                        <div className="progress-over" style={carbStyle}>
+                        <div className='progress-over' style={carbStyle}>
                           {carbsProgress}%
                         </div>
                       ) : (
-                        <div className="progress-done" style={carbStyle}>
+                        <div className='progress-done' style={carbStyle}>
                           {carbsProgress}%
                         </div>
                       )}
                     </div>
-                  </Card.Text>
-                  <Card.Text className=" fs-5 my-3">
-                    <strong>Fats: </strong>
-                    {todaysFats} grams consumed. <br></br>{' '}
-                    <span className="text-lowercase">
-                      {deficitOrSurplus(userFats - todaysFats, 'fat')}
-                    </span>
-                    <div className="text-lowercase mt-3">
-                      {fatRecs.length
-                        ? 'Recommended foods to meet fat goals for today:'
-                        : null}
-                    </div>
-                    <div>
-                      {fatRecs.length ? (
-                        <ul>
-                          {fatRecs.map((foodItem) => {
-                            return (
-                              <>
-                                <span className="fs-6 text-lowercase">
-                                  {
-                                    foodItem[Object.keys(foodItem)[0]]
-                                      .servingSize
-                                  }{' '}
-                                  {Object.keys(foodItem)[0]} has{' '}
-                                  {foodItem[Object.keys(foodItem)[0]].fats} g
-                                </span>
-                                <br></br>
-                              </>
-                            );
-                          })}
-                        </ul>
-                      ) : null}
-                    </div>
-                    <br></br>
-                    <div className="progress">
-                      {fatsProgress > 100 ? (
-                        <div className="progress-over" style={fatStyle}>
-                          {fatsProgress}%
-                        </div>
-                      ) : (
-                        <div className="progress-done" style={fatStyle}>
-                          {fatsProgress}%
-                        </div>
-                      )}
-                    </div>
-                  </Card.Text>
-                  <Card.Text className=" fs-5 my-3">
-                    <strong>Proteins: </strong> {todaysProteins} grams consumed.{' '}
-                    <br></br>
-                    <span className="text-lowercase">
-                      {deficitOrSurplus(
-                        userProteins - todaysProteins,
-                        'protein'
-                      )}
-                    </span>
-                    <div className="text-lowercase mt-3">
-                      {proteinRecs.length
-                        ? 'Recommended foods to meet protein goals for today:'
-                        : null}
-                    </div>
-                    <div>
-                      {proteinRecs.length ? (
-                        <ul>
-                          {proteinRecs.map((foodItem) => {
-                            return (
-                              <>
-                                <span className="fs-6 text-lowercase">
-                                  {
-                                    foodItem[Object.keys(foodItem)[0]]
-                                      .servingSize
-                                  }{' '}
-                                  {Object.keys(foodItem)[0]} has{' '}
-                                  {foodItem[Object.keys(foodItem)[0]].protein} g
-                                </span>
-                                <br></br>
-                              </>
-                            );
-                          })}
-                        </ul>
-                      ) : null}
-                    </div>
-                    <br></br>
-                    <div className="progress">
-                      {proteinsProgress > 100 ? (
-                        <div className="progress-over" style={proteinStyle}>
-                          {proteinsProgress}%
-                        </div>
-                      ) : (
-                        <div className="progress-done" style={proteinStyle}>
-                          {proteinsProgress}%
-                        </div>
-                      )}
-                    </div>
-                  </Card.Text>
-                </div>
+                  </span>
+                  <span className='text-lowercase fs-6'>
+                    {deficitOrSurplus(userCarbs - todaysCarbs, 'carb')}{' '}
+                  </span>
+                  <br></br>
+                  <div className='text-lowercase mt-1 fw-bold fs-6'>
+                    {carbRecs.length
+                      ? 'Recommended foods to meet daily carb goals:'
+                      : null}
+                  </div>
+                  <div>
+                    {carbRecs.length ? (
+                      <ul>
+                        {carbRecs.map((foodItem) => {
+                          return (
+                            <>
+                              <span className='fs-6 text-lowercase'>
+                                {foodItem[Object.keys(foodItem)[0]].servingSize}{' '}
+                                {Object.keys(foodItem)[0]} has{' '}
+                                {foodItem[Object.keys(foodItem)[0]].carbs} g
+                              </span>
+                              <br></br>
+                            </>
+                          );
+                        })}
+                      </ul>
+                    ) : null}
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className='mt-3 mb-5' style={{ height: '25rem' }}>
+          <Col>
+            <Card>
+              <CardHeader>
+                <Card.Text className='fw-bold fs-6 text-center my-2'>
+                  Fats
+                </Card.Text>
+              </CardHeader>
+              <Card.Body>
+                <Card.Text className=' fs-5 my-3'>
+                  <strong>Consumed: </strong>
+                  {todaysFats} grams
+                  <div className='progress mx-auto'>
+                    {fatsProgress > 100 ? (
+                      <div className='progress-over' style={fatStyle}>
+                        {fatsProgress}%
+                      </div>
+                    ) : (
+                      <div className='progress-done' style={fatStyle}>
+                        {fatsProgress}%
+                      </div>
+                    )}
+                  </div>
+                  <span className='text-lowercase fs-6'>
+                    {deficitOrSurplus(userFats - todaysFats, 'fat')}
+                  </span>
+                  <div className='text-lowercase mt-1 fw-bold fs-6'>
+                    {fatRecs.length
+                      ? 'Recommended foods to meet daily fat goals:'
+                      : null}
+                  </div>
+                  <div>
+                    {fatRecs.length ? (
+                      <ul>
+                        {fatRecs.map((foodItem) => {
+                          return (
+                            <>
+                              <span className='fs-6 text-lowercase'>
+                                {foodItem[Object.keys(foodItem)[0]].servingSize}{' '}
+                                {Object.keys(foodItem)[0]} has{' '}
+                                {foodItem[Object.keys(foodItem)[0]].fats} g
+                              </span>
+                              <br></br>
+                            </>
+                          );
+                        })}
+                      </ul>
+                    ) : null}
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            {/* // */}
+            <Card>
+              <CardHeader>
+                <Card.Text className='fw-bold fs-6 text-center my-2'>
+                  Proteins
+                </Card.Text>
+              </CardHeader>
+              <Card.Body>
+                <Card.Text className=' fs-5 my-3'>
+                  <strong>Consumed:</strong> {todaysProteins} grams
+                  <div className='progress mx-auto'>
+                    {proteinsProgress > 100 ? (
+                      <div className='progress-over' style={proteinStyle}>
+                        {proteinsProgress}%
+                      </div>
+                    ) : (
+                      <div className='progress-done' style={proteinStyle}>
+                        {proteinsProgress}%
+                      </div>
+                    )}
+                  </div>
+                  <span className='text-lowercase fs-6'>
+                    {deficitOrSurplus(userProteins - todaysProteins, 'protein')}
+                  </span>
+                  <div className=' text-lowercase mt-1 fw-bold fs-6'>
+                    {proteinRecs.length
+                      ? 'Recommended foods to meet daily protein goals:'
+                      : null}
+                  </div>
+                  <div>
+                    {proteinRecs.length ? (
+                      <ul>
+                        {proteinRecs.map((foodItem) => {
+                          return (
+                            <>
+                              <span className='fs-6 text-lowercase'>
+                                {foodItem[Object.keys(foodItem)[0]].servingSize}{' '}
+                                {Object.keys(foodItem)[0]} has{' '}
+                                {foodItem[Object.keys(foodItem)[0]].protein} g
+                              </span>
+                              <br></br>
+                            </>
+                          );
+                        })}
+                      </ul>
+                    ) : null}
+                  </div>
+                </Card.Text>
               </Card.Body>
             </Card>{' '}
           </Col>
         </Row>
+        <Col></Col>
       </Container>
     </>
   );
